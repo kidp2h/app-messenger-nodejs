cb649f54268ff7fe3d52910eda3573c26058d90f
kidp2h <thjnhsoajca@gmail.com>
2020-02-17 00:01:51 +0700

    messenger

--- models/user.model.js
+++ models/user.model.js
@@ -1,1 +1,69 @@
-import  mongoose from "mongoose"
+import mongoose from "mongoose"
+let Schema = mongoose.Schema;
+let UserSchema = new Schema({
+    username: String,
+    gender: {
+        type: String,
+        default: "male"
+    },
+    phone: {
+        type: Number,
+        default: null
+    },
+    address: {
+        type: String,
+        default: null
+    },
+    avatar: {
+        type: String,
+        default: "defaultUser.png"
+    },
+    role: {
+        type: String,
+        default: "user"
+    },
+    local: {
+        email: {
+            type: String,
+            trim: true
+        },
+        password: String,
+        isActive: {
+            type: Boolean,
+            default: false
+        },
+        verifyToken: {
+            type: String,
+            default: null
+        }
+    },
+    facebook: {
+        uid: String,
+        token: String,
+        email: {
+            type: String,
+            trim: true
+        }
+    },
+    google: {
+        uid: String,
+        token: String,
+        email: {
+            type: String,
+            trim: true
+        }
+    },
+    createdAt: {
+        type: Number,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Number,
+        default: null
+    },
+    deletedAt: {
+        type: Number,
+        default: null
+    }
+});
+module.exports = mongoose.model("user",UserSchema)
