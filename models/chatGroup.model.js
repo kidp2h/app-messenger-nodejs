bd1680a62a6fcf578c1860c46b2c28bb6ba04bbf
kidp2h <thjnhsoajca@gmail.com>
2020-02-17 00:26:18 +0700

    messenger

---
+++ models/chatGroup.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+let Schema = mongoose.Schema;
+let ChatGroupSchema = new Schema({
+    id: String,
+    name: String,
+    usersAmount: {
+        type: Number,
+        min: 3,
+        max: 15
+    },
+    messageAmount: {
+        type: Number,
+        default: 0
+    },
+    userId: String,
+    members: [{
+        userId: String
+    }],
+    createdAt: {
+        type: Number,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Number,
+        default: null
+    },
+    deletedAt: {
+        type: Number,
+        default: null
+    }
+});
+module.exports = mongoose.model("chat-group",ChatGroupSchema)
